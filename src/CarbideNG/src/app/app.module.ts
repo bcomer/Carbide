fe3f5c6d2d36d4083547937676e5c1af36ecabda
bcomer <blcomer79@gmail.com>
2019-09-28 18:06:17 -0500

    created the beginings of the projects tree

--- src/CarbideNG/src/app/app.module.ts
+++ src/CarbideNG/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent
   ],
   imports: [
     BrowserModule,
