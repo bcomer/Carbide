eca4198723a6dbc21e15e34c711caf8626924738
bcomer <blcomer79@gmail.com>
2019-10-03 19:42:46 -0500

    added project model and the start of the project service

---
+++ src/CarbideNG/src/app/models/project.ts
@@ -0,0 +1,8 @@
+export class Project {
+    constructor(
+        public id?: string,
+        public name?: string,
+        public createdBy?: string,
+        public createdOn?: string
+    ) {}
+}
--- src/CarbideNG/src/app/services/project.service.ts
+++ src/CarbideNG/src/app/services/project.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
+import { Project } from '../models/project';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,43 @@ import { Injectable } from '@angular/core';
 export class ProjectService {
 
   constructor() { }
+
+  getAll(): Observable<Array<Project>> {
+    return of(new Array<Project>());
+  }
+
+  save(entity: Project): Observable<Project> {
+    return of(entity);
+  }
+
+  delete(id: string): Observable<Project> {
+    return of();
+  }
+}
+
+const PROJECT_DATA = {
+  Projects: [
+    {
+      ProjectOne: {
+        Calculations: ['CalcOne', 'CalcFour', 'CalcFive'],
+        Projects: [
+          {
+            SubprojectOne: {
+              Calculations: ['CalcTwo', 'CalcThree']
+            }
+          },
+          {
+            SubprojectTwo: {
+              Calculations: ['CalcTwo', 'CalcThree']
+            }
+          }
+        ]
+
+      },
+      ProjectTwo: {
+        Calculations: ['CalcA', 'CalcB'],
+        Projects: null
+      }
+    }
+  ]
 }
