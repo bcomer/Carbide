b28841a3dd8c1a2ef0b0f6d0c243d66fb19fef1f
Britany <britany.a.kaiser@gmail.com>
2020-07-20 21:44:30 -0500

    nom pipe size select

--- src/CarbideNG/src/app/calculation-details/calculation-details.component.ts
+++ src/CarbideNG/src/app/calculation-details/calculation-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Api5L } from '../models/api-5l';
+import { map, mapTo } from 'rxjs/operators';
 
 @Component({
   selector: 'cbd-calculation-details',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculation-details.component.scss']
 })
 export class CalculationDetailsComponent implements OnInit {
+private nominalPipeSize:Api5L[]=[];
 
-  constructor() { }
+  constructor(private readonly fireStore: AngularFirestore ) {this.getApi5LValues(); }
 
   ngOnInit() {
+    
   }
+
+  getApi5LValues(){
+    this.fireStore.collection<Api5L>('Api5L').get().forEach(a => a.docs.forEach(b => {this.nominalPipeSize.push(b.data() as Api5L);}));
+  } 
 
 }
---
+++ src/CarbideNG/src/app/models/api-5l.ts
@@ -0,0 +1,10 @@
+import { WallThickness } from './wall-thickness';
+
+export class Api5L{
+    constructor(
+        public nominalPipeSize: number,
+        public nominalOd: number,
+        public wallThickness: WallThickness[]
+    
+    ) {}
+}
