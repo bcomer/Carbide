68999d9f047c5f5cf641a52756e11e5c210abcfb
Brandon Comer <blcomer79@gmail.com>
2020-09-13 20:50:50 -0500

    messing around

--- src/CarbideNG/src/app/shared/form-builder/form-builder.module.ts
+++ src/CarbideNG/src/app/shared/form-builder/form-builder.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DesignPressureSteelPipeComponent } from './design-pressure-steel-pipe/design-pressure-steel-pipe.component';
 import { CalculationDetailsComponent } from './calculation-details/calculation-details.component';
-
+import { ModuleSelectorComponent } from './module-selector/module-selector.component';
+import { ModuleListComponent } from './module-list/module-list.component';
 
 
 @NgModule({
-  declarations: [DesignPressureSteelPipeComponent, CalculationDetailsComponent],
+  declarations: [DesignPressureSteelPipeComponent, CalculationDetailsComponent, ModuleSelectorComponent, ModulesListComponent, ModuleListComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CalculationDetailsComponent
   ]
 })
 export class FormBuilderModule { }
---
+++ src/CarbideNG/src/app/shared/form-builder/module-selector/module-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cbd-module-selector',
+  templateUrl: './module-selector.component.html',
+  styleUrls: ['./module-selector.component.scss']
+})
+export class ModuleSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
