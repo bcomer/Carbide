fdc5837d5184720e54fdd30d238ca8d87f359468
Brandon Comer <blcomer79@gmail.com>
2020-11-21 15:50:20 -0600

    styled calculation items

--- src/CarbideNG/src/app/calculation-item/calculation-item.component.ts
+++ src/CarbideNG/src/app/calculation-item/calculation-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Calculation } from '../models/calculation';
 
 @Component({
   selector: 'cbd-calculation-item',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculation-item.component.scss']
 })
 export class CalculationItemComponent implements OnInit {
+  @Input() calculation: Calculation;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  isValid(): boolean {
+    return this.calculation.isValid;
   }
 
 }
